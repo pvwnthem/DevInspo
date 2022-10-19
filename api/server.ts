5f2478bdac54d92f0642039ced6067254030811c
pvwnthem <pvwn.them@gmail.com>
2022-10-18 21:55:12 -0400

    add static files on express

--- api/server.ts
+++ api/server.ts
@@ -1,12 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import posts from './routes/posts'
+import path from 'path'
 const app = express()
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/posts', posts)
 const port = process.env.PORT || 8000
+
+
+app.use(express.static('build'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
+})
+
 
 app.listen(port, () => {
     console.log('listening on ', port)
