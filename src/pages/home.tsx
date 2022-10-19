7ec8f64e3b3d087e615e35a75c1a43b8877e2cf2
pvwnthem <pvwn.them@gmail.com>
2022-10-19 18:06:56 -0400

    add cors and axios

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Post from '../components/post'
 import Submit from './submit'
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 export default function Home() {
+    const [post, setPost] = useState([])
+
+    const getPost = async () => {
+        const {data} = await axios.get('http://localhost:8000/api/v1/posts/random')
+        setPost(data)
+    }
+    useEffect(() => {
+        getPost();
+      }, []);
     return (
         <>
            
-            < Post />
+            < Post title={JSON.stringify(post)} />
        </> 
     )
 }
