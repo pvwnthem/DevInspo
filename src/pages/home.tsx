a15c97039cf5ea25a141282b339d505c9a3228fe
pvwnthem <pvwn.them@gmail.com>
2022-10-19 20:10:28 -0400

    omg bro cors

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,7 +12,10 @@ export default function Home() {
     })
 
     const getPost = async () => {
-        const {data} = await axios.get('http://localhost:8000/api/v1/posts/random')
+        const client = axios.create({
+            baseURL: 'http://localhost:8080'
+        })
+        const {data} = await client.get('/api/v1/posts/random')
         setPost(data)
     }
     useEffect(() => {
