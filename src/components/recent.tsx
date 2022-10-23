1ae790c723d5687a6b2958b17170223102d677c9
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 11:04:07 -0400

    ready url for prod

--- src/components/recent.tsx
+++ src/components/recent.tsx
@@ -6,7 +6,7 @@ export default function Recent(props: any) {
     const [data, setData] = useState('')
     
     const getPosts = async () => {
-        const {data} = await axios.get('http://localhost:8000/api/v1/posts/recent')
+        const {data} = await axios.get('https://codeideas.herokuapp.com/api/v1/posts/recent')
         const json = data
         const arr = json.map(
             (x: any) => < RecentPost title={JSON.stringify(x.title).replace(/"/g, "")} text= {JSON.stringify(x.text).replace(/"/g, "")} id= {JSON.stringify(x.id).replace(/"/g, "")} likes = {JSON.stringify(x.likes)}/> 
