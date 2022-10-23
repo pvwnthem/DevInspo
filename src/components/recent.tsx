0d40e2433fb83e15c75959807b21ad3be4fe402d
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 11:08:27 -0400

    add limit useState

--- src/components/recent.tsx
+++ src/components/recent.tsx
@@ -4,9 +4,9 @@ import { useState, useEffect } from 'react'
 import RecentPost from './RecentPost'
 export default function Recent(props: any) {
     const [data, setData] = useState('')
-    
+    const [limit, setLimit] = useState(5)
     const getPosts = async () => {
-        const {data} = await axios.get('https://codeideas.herokuapp.com/api/v1/posts/recent')
+        const {data} = await axios.get(`https://codeideas.herokuapp.com/api/v1/posts/recent?limit=${limit}`)
         const json = data
         const arr = json.map(
             (x: any) => < RecentPost title={JSON.stringify(x.title).replace(/"/g, "")} text= {JSON.stringify(x.text).replace(/"/g, "")} id= {JSON.stringify(x.id).replace(/"/g, "")} likes = {JSON.stringify(x.likes)}/> 
