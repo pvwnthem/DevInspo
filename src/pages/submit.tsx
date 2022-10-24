fe141f6f3ecd614957b42cb28738f58124064985
pvwnthem <pvwn.them@gmail.com>
2022-10-23 20:51:19 -0400

    test nav

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -1,52 +1,42 @@
 import React, { Component } from 'react';
 import ExamplePost from '../components/examplePost'
 import { useState } from 'react'
-
-import { useNavigate  } from "react-router-dom"
-import { Function } from 'estree';
-
+import  Redirect, { Navigate }  from 'react-router-dom';
+import {Link, Routes, Route, useNavigate} from 'react-router-dom';
 
 
-class Submit extends Component<{ nav?: any }>{
-    state = {
-        title: '',
-        text: '',
-        chars: 0
-        
-       
-    };
-    content = {
-        
-    }
-    handleChange = (e: any) => {
-        this.setState({[e.target.n]:e.target.v})
-    }
+
+
+export default function Submit(){
     
-    handleSubmit = (e: any) => {
-        e.preventDefault();
+    const [text, setText] = useState('')
+    const [chars, setChars] = useState(0)
+    const [title, setTitle] = useState('')
+    
+
+    const navigate = useNavigate()
+    const handleSubmit = (e: any) => {
+        console.log('form submitted')
+        e.preventDefault()
         
-        this.props.nav.navigate('/');
-    }
-    render() {
+        
+   }
+    
         
 
         
         return (
         <div className='w-full h-full'>
 
-            <form action='/api/v1/posts/new' method='post' className='mx-auto flex flex-col items-center'>
-                <input name="title" type="text" className=" md:w-1/3 w-full mt-24 border border-black rounded px-2 py-2 " maxLength={128} placeholder='Your Submissions Title'  onChange={e => {this.setState({
-                    title: e.target.value
-                })
+            <form action='https://codeideas.herokuapp.com/api/v1/posts/new' method='post' className='mx-auto flex flex-col items-center'>
+                <input name="title" type="text" className=" md:w-1/3 w-full mt-24 border border-black rounded px-2 py-2 " maxLength={128} placeholder='Your Submissions Title'  onChange={e => {
+                    setTitle(e.target.value)
                
                 }
                 
                 }></input>
-                <textarea name="text" placeholder='Your Submissions Body'  className='border border-black rounded h-56 md:w-1/3 w-full py-2 px-2 max-w-full' maxLength={1024} onChange={b => {this.setState({
-                    
-                    text: b.target.value
-                })}}></textarea>
-                <button type="submit">Submit</button>
+                <textarea name="text" placeholder='Your Submissions Body'  className='border border-black rounded h-56 md:w-1/3 w-full py-2 px-2 max-w-full' maxLength={1024} onChange={b => {setText(b.target.value)}}></textarea>
+                <button type="submit" onClick={() => navigate('/')}>Submit</button>
             </form>
 
 
@@ -55,15 +45,8 @@ class Submit extends Component<{ nav?: any }>{
 
 
 
-            <ExamplePost title={this.state.title} text={this.state.text} titleLimit={128} textLimit = {1024}/>
+            <ExamplePost title={title} text={text} titleLimit={128} textLimit = {1024}/>
         
         </div>
         )
-    }
-}
-
-export function SubWithRouter(props: any) {
-    const navigate = useNavigate()
-    return (<Submit nav={navigate}/>)
-}
-export default SubWithRouter
+            }
