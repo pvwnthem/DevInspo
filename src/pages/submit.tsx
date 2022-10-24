a9e4b48fe87f933fe46b82eee6dcf584a7c08424
pvwnthem <pvwn.them@gmail.com>
2022-10-24 19:28:38 -0400

    another test

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -34,7 +34,7 @@ export default function Submit(){
         } catch(error) {
           console.log(error)
         }
-         navigate('/')
+         navigate('/random')
       }
     
       const handleChange = (event: any) => {
