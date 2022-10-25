c768e6be7345df40000b81fd6ce6a9693941cb6d
pvwnthem <pvwn.them@gmail.com>
2022-10-25 02:16:43 -0400

    e

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -76,7 +76,7 @@ export default function Navbar() {
                         </ul>
                     </div>
                 </div>
-                <div className='flex md:flex-col w-full md:visible invisible  absolute md:mt-56 h-12 '>
+                <div className='flex md:flex-col w-48 md:visible invisible  absolute md:mt-56 h-12 '>
                 <ol>    
                 <li className='text-2xl text-black  md:mt-48 mt-4'>Home</li>
                 <li className='text-2xl text-black  mt-4'>About</li>
