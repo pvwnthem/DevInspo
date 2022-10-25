742181010ec47682f9a25d6fa0b377a35b071270
pvwnthem <pvwn.them@gmail.com>
2022-10-24 22:14:42 -0400

    re

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -55,7 +55,6 @@ export default function Submit(){
         
           // make axios post request
           await axios.get(`/api/v1/posts/new?title=${formValue.title}&text=${formValue.text}`)
-            navigate('https://codeideas.herokuapp.com')
          
           
         } catch(error) {
