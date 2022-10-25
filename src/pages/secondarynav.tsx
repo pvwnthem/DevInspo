1e31de2dcd350a79bde80de6178ef5e9cb7f6741
pvwnthem <pvwn.them@gmail.com>
2022-10-25 01:33:20 -0400

    remove rising for now

--- src/pages/secondarynav.tsx
+++ src/pages/secondarynav.tsx
@@ -10,7 +10,6 @@ export default function SecondaryNav(props: any) {
     const thisPage = props.page
     const [recent, setRecent] = useState(false)
     const [popular, setPopular] = useState(false)
-    const [Rising, setRising] = useState(false)
     const [oldest, setOldest] = useState(false)
     const [home, setHome] = useState(false)
     const [done, setDone] = useState(false)
@@ -30,10 +29,7 @@ export default function SecondaryNav(props: any) {
             setHome(false)
             setPopular(true)
         }
-        if (props.page === 'rising') {
-            setHome(false)
-            setRising(true)
-        }
+        
         if (props.page === 'oldest') {
             setHome(false)
             setOldest(true)
@@ -59,9 +55,7 @@ export default function SecondaryNav(props: any) {
                   <li className="w-full">
                       <a href="/?page=popular" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Popular</a>
                   </li>
-                  <li className="w-full">
-                      <a href="/?page=rising" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Rising</a>
-                  </li>
+                  
                   <li className="w-full">
                       <a href="/?page=oldest" className="inline-block px-6 py-3 w-full text-gray-900 bg-white rounded-r-full hover:text-black hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Oldest</a>
                   </li>
@@ -90,9 +84,7 @@ export default function SecondaryNav(props: any) {
                   <li className="w-full">
                       <a href="/?page=popular" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Popular</a>
                   </li>
-                  <li className="w-full">
-                      <a href="/?page=rising" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Rising</a>
-                  </li>
+                  
                   <li className="w-full">
                       <a href="/?page=oldest" className="inline-block px-6 py-3 w-full text-gray-900 bg-white rounded-r-full hover:text-black hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Oldest</a>
                   </li>
@@ -121,9 +113,7 @@ export default function SecondaryNav(props: any) {
                   <li className="w-full">
                       <a href="/?page=popular" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Popular</a>
                   </li>
-                  <li className="w-full">
-                      <a href="/?page=rising" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Rising</a>
-                  </li>
+                  
                   <li className="w-full">
                       <a href="/?page=oldest" className="inline-block px-6 py-3 w-full text-gray-900 bg-white rounded-r-full hover:text-black hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Oldest</a>
                   </li>
@@ -136,35 +126,8 @@ export default function SecondaryNav(props: any) {
             
         </div>
     ) : null }
-    { Rising ? (
-        <div className='w-full h-full '>
-            {! done ? (
-                <Loading />
-            ): (
-                <div className='w-full h-full '>
-                <div className='flex'>
-                <ul className="mx-auto flex text-sm font-medium text-center text-gray-500 rounded-full divide-x divide-gray-200 border-2 hover:text-black active:bg-blue-700 dark:divide-gray-700 dark:text-gray-400 mt-24">
-                  <li className="w-full">
-                      <a href="/?page=recent" className="inline-block px-6 py-3 w-full text-gray-900 hover:text-black rounded-l-full hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Recent</a>
-                  </li>
-                  <li className="w-full">
-                      <a href="/?page=popular" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Popular</a>
-                  </li>
-                  <li className="w-full">
-                      <a href="/?page=rising" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Rising</a>
-                  </li>
-                  <li className="w-full">
-                      <a href="/?page=oldest" className="inline-block px-6 py-3 w-full text-gray-900 bg-white rounded-r-full hover:text-black hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Oldest</a>
-                  </li>
-              </ul>  
-                </div>
-              < Risinge />
-          </div>  
-            
-            )}
-            
-        </div>
-    ) : null }
+    
+    
 
     { oldest ? (
         <div className='w-full h-full '>
@@ -180,9 +143,7 @@ export default function SecondaryNav(props: any) {
                   <li className="w-full">
                       <a href="/?page=popular" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Popular</a>
                   </li>
-                  <li className="w-full">
-                      <a href="/?page=rising" className="inline-block px-6 py-3 w-full text-gray-900 bg-white hover:text-black hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Rising</a>
-                  </li>
+                  
                   <li className="w-full">
                       <a href="/?page=oldest" className="inline-block px-6 py-3 w-full text-gray-900 bg-white rounded-r-full hover:text-black bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Oldest</a>
                   </li>
