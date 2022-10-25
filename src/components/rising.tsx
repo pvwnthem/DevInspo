5eb75af0a89ab1a2366122916bf8dc297f378ad6
pvwnthem <pvwn.them@gmail.com>
2022-10-25 01:02:15 -0400

    antoher test with rising

--- src/components/rising.tsx
+++ src/components/rising.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import RecentPost from './RecentPost'
+import RisingPost from './RisingPost'
 export default function Oldest(props: any) {
     const [data, setData] = useState('')
-    const [limit, setLimit] = useState(5)
+    const [limit, setLimit] = useState(100)
     const getPosts = async () => {
         const {data} = await axios.get(`https://codeideas.herokuapp.com/api/v1/posts/rising?limit=${limit}`)
         const json = data
         let newarr: any = []
         
-        json.forEach((doc:any) => {
-            if (doc.likes > 0) {
-                newarr.push(doc)
-            
-            }
-        })
-        const arr = newarr.map(
-            (x: any) => < RecentPost title={JSON.stringify(x.title).replace(/"/g, "")} text= {JSON.stringify(x.text).replace(/"/g, "")}tags = {JSON.stringify( x.tags).replace(/[\[\]"]+/g,'').replace(/,+/g, ' #')} id= {JSON.stringify(x.id).replace(/"/g, "")} likes = {JSON.stringify(x.likes)}/> 
+        
+        const arr = json.map(
+            (x: any) => < RisingPost title={JSON.stringify(x.title).replace(/"/g, "")} text= {JSON.stringify(x.text).replace(/"/g, "")}tags = {JSON.stringify( x.tags).replace(/[\[\]"]+/g,'').replace(/,+/g, ' #')} id= {JSON.stringify(x.id).replace(/"/g, "")} likes = {JSON.stringify(x.likes)}/> 
         )
         console.log(arr)
         return arr;
